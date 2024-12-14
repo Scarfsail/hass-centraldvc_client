fa6e4c908e133f4325fed71160c665b07f47a0b8
Ondrej Salplachta <ondrej@salplachta.net>
2024-12-14 13:51:45 +0000

    Hw_control now fully works including services

--- hw_control/__init__.py
+++ hw_control/__init__.py
@@ -4,25 +4,19 @@ from homeassistant.core import HomeAssistant
 from .const import DOMAIN  # Define DOMAIN in const.py
 
 
-async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    """Set up the integration from configuration.yaml (if used)."""
-    hass.data.setdefault(DOMAIN, {})
-    return True
-
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up the integration from a config entry."""
     # Forward the entry setup to the switch platform
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "switch")
-    )
+    # hass.data.setdefault(DOMAIN, {})
+    await hass.config_entries.async_forward_entry_setup(entry, "select")
+
     return True
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Handle unloading of a config entry."""
     # Remove platform setup
-    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "switch")
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "select")
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id, None)
     return unload_ok
---
+++ hw_control/select.py
@@ -0,0 +1,161 @@
+import voluptuous as vol
+
+from homeassistant.components.select import SelectEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers import config_validation as cv, entity_platform
+from homeassistant.helpers.event import async_track_state_change
+
+TXT_ON = "ZAP"
+TXT_OFF = "VYP"
+TXT_AUTO = "AUTO"
+
+
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities
+):
+    """Set up the switch platform from a config entry."""
+    name = entry.data["name"]
+    linked_entity = entry.data["linked_entity"]
+    entity = HwControl(name, linked_entity, entry)
+
+    async_add_entities([entity])
+
+    register_services()
+
+    return True
+
+
+def register_services():
+    def set_value_when_auto(entity: HwControl, service_call):
+        entity.set_value_when_auto(service_call.data["value"])
+
+    def turn_on(entity: HwControl, service_call):
+        entity.select_option(TXT_ON)
+
+    def turn_off(entity: HwControl, service_call):
+        entity.select_option(TXT_OFF)
+
+    def set_auto(entity: HwControl, service_call):
+        entity.select_option(TXT_AUTO)
+
+    platform = entity_platform.async_get_current_platform()
+
+    platform.async_register_entity_service(
+        "set_value_when_auto",
+        {vol.Required("value"): cv.boolean},
+        set_value_when_auto,
+    )
+
+    platform.async_register_entity_service(
+        "turn_on",
+        None,
+        turn_on,
+    )
+
+    platform.async_register_entity_service(
+        "turn_off",
+        None,
+        turn_off,
+    )
+
+    platform.async_register_entity_service(
+        "set_auto",
+        None,
+        set_auto,
+    )
+
+
+class HwControl(SelectEntity):
+    """Representation of a HwControl entity."""
+
+    def __init__(self, name: str, linked_entity: str, entry) -> None:
+        """Initialize the HwControl."""
+        self._config_entry = entry
+        self._name = name
+        self._linked_entity = linked_entity
+        self._is_auto_active = False
+        self._value_when_auto = False
+
+    async def async_added_to_hass(self) -> None:
+        """Run when entity is added to Home Assistant."""
+        # Track state changes for the linked switch
+        async_track_state_change(
+            self.hass, self._linked_entity, self._handle_linked_entity_change
+        )
+
+    async def _handle_linked_entity_change(self, entity_id, old_state, new_state):
+        """Handle state changes for the linked switch."""
+        # Notify Home Assistant of a state update
+        self.schedule_update_ha_state()
+
+    @property
+    def name(self) -> str:
+        """Return the name of the switch."""
+        return self._name
+
+    @property
+    def options(self):
+        return [TXT_ON, TXT_OFF, TXT_AUTO]
+
+    @property
+    def current_option(self):
+        if self._is_auto_active:
+            return TXT_AUTO
+
+        if self.is_on:
+            return TXT_ON
+
+        return TXT_OFF
+
+    def select_option(self, option: str):
+        if option == TXT_OFF:
+            self.turn_off()
+            self._is_auto_active = False
+        if option == TXT_ON:
+            self.turn_on()
+            self._is_auto_active = False
+        if option == TXT_AUTO:
+            self._is_auto_active = True
+            if self._value_when_auto:
+                self.turn_on()
+            else:
+                self.turn_off()
+
+    def set_value_when_auto(self, value):
+        if self._value_when_auto == value:
+            return
+
+        self._value_when_auto = value
+        if self._is_auto_active:
+            if value:
+                self.turn_on()
+            else:
+                self.turn_off()
+        self.schedule_update_ha_state()
+
+    @property
+    def is_on(self) -> bool:
+        """Return the state of the switch."""
+        state = self.hass.states.get(self._linked_entity)
+        if state:
+            return state.state == "on"
+        return False
+
+    def turn_on(self) -> None:
+        """Turn on the switch."""
+        domain = self.get_domain(self._linked_entity)
+        self.hass.services.call(domain, "turn_on", {"entity_id": self._linked_entity})
+        # self._is_on = True
+        self.schedule_update_ha_state()
+
+    def turn_off(self) -> None:
+        """Turn off the switch."""
+        domain = self.get_domain(self._linked_entity)
+        self.hass.services.call(domain, "turn_off", {"entity_id": self._linked_entity})
+        # self._is_on = False
+        self.schedule_update_ha_state()
+
+    def get_domain(self, entity_id):
+        """Get domain from entity ID."""
+        return entity_id.split(".")[0]
